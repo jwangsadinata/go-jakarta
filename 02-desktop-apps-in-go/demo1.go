5f4ba64d2c21c7ec88c0af86d8b4c16681339e0c
Jason Wangsadinata <jwangsadinata@gmail.com>
2018-08-28 14:07:41 +0700

    new-talk: add desktop apps in golang slides and demo

---
+++ 02-desktop-apps-in-go/demo1.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/pixelgl"
+)
+
+func run() {
+	cfg := pixelgl.WindowConfig{
+		Title:  "Demo 1",
+		Bounds: pixel.R(0, 0, 1024, 768),
+		VSync:  true,
+	}
+	win, err := pixelgl.NewWindow(cfg)
+	if err != nil {
+		panic(err)
+	}
+
+	for !win.Closed() {
+		win.Update()
+	}
+}
+
+func main() {
+	pixelgl.Run(run)
+}
